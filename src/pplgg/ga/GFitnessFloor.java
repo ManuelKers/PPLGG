fc1d58801536c854b27feeb7dcc6bcddf00d150d
Manuel Kers <m.kerssemakers@gmail.com>
2011-11-30 14:41:41 +0100

    changed GFitnessFloor

--- src/pplgg/ga/GFitnessFloor.java
+++ src/pplgg/ga/GFitnessFloor.java
@@ -7,20 +7,6 @@ import ga.Individual;
 
 public class GFitnessFloor extends GeneratorFitness {
 
-    @Override
-    public double evaluateMap( Map map ) {
-        int w = map.getWidth();
-        int h = map.getHeight();
-        int noBottomSolid = 0;
-        int noTopSolid = 0;
-        for (int x=0; x<w; x++) 
-            for (int y=0; y<h; y++)
-                if (y>1*h/3)
-                    noBottomSolid += (map.getTerrain( x, y ) == Terrain.SOLID) ? 1 : 0;
-                else
-                    noTopSolid += (map.getTerrain( x, y ) == Terrain.SOLID) ? 1 : 0;
-        return (w*h + (noBottomSolid - noTopSolid));
-    }
 	@Override
 	public double evaluateMap( Map map ) {
 		int w = map.getWidth();
